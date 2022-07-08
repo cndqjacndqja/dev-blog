51c8ebe384a4b370bcdde3ebb7fcb83c74a45dd7
intae92 <kangit2013@gmail.com>
2022-07-08 13:26:08 +0900

    Updates

---
+++ component---src-pages-404-js-34b51c11e0c278d141fc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgong_check_dev_blog=self.webpackChunkgong_check_dev_blog||[]).push([[883],{429:function(e,t,n){n.r(t);var a=n(7294),l=n(8678),o=n(262);t.default=function(e){var t=e.data,n=e.location,c=t.site.siteMetadata.title;return a.createElement(l.Z,{location:n,title:c},a.createElement(o.Z,{title:"404: Not Found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-34b51c11e0c278d141fc.js.map
